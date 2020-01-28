cf9d1cd790a49b65d0dfa5576888041dcccbfef2
Your Name <you@example.com>
2020-01-28 13:57:15 +0200

    fix

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 "use strict"
 
-// у нас текстовое поле, списк и кнопка, при сохранении новой задачи 
-// она появляется в списке ниже
-// рядом с каждой задачей есть чекбокс, 
-// если его нажать, то задача становится выполненной и текст 
-// должен быть зачеркнутым
-// также рядом с каждой задачей есть крестик, 
-// при клике на который задача удаляется из списка
-
 let formSubmit = document.querySelector('form');
 let itemsArray = [];
 let todoList = document.querySelector('.toDoContent')
@@ -16,7 +8,7 @@ let itemIdNumber = 0;
 console.log(formSubmit);
 console.log(todoList);
 
-formSubmit.addEventListener('submit', function (e) {
+formSubmit.addEventListener('submit', function(e) {
     e.preventDefault();
     itemIdNumber++;
     let newItem = document.querySelector('input').value
@@ -35,36 +27,29 @@ formSubmit.addEventListener('submit', function (e) {
     itemCheck.classList.add('checkThis');
     closeBlock.classList.add('close');
 
-    oneItem.classList.add('item');   
-    itemText.innerHTML =  newItem;
+    oneItem.classList.add('item');
+    itemText.innerHTML = newItem;
 
     todoList.append(oneItem);
     oneItem.prepend(itemCheck);
     oneItem.append(itemText);
     oneItem.append(closeBlock);
 
-    itemCheck.addEventListener('click', function(){
+    itemCheck.addEventListener('click', function() {
 
-        if(this.closest('.item').classList.contains('checkd')){
+        if (this.closest('.item').classList.contains('checkd')) {
             this.closest('.item').classList.remove('checkd');
             console.log('remove class')
         } else {
             this.closest('.item').classList.add('checkd');
             console.log('add class')
         }
-       }) 
+    })
 
-    closeBlock.addEventListener('click', function(){
+    closeBlock.addEventListener('click', function() {
 
         this.closest('li').style.display = "none"
 
-    }) 
+    })
 
 });
-
-
-
-
-
-
-
